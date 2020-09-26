1471c3cab8552e64b33c6fe361de95d3fb22030f
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-09-26 16:31:24 -0300

    feat(index.js): db connection

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 import express from 'express';
 import { medicineRouter } from './routes/medicineRouter.js';
 
+import { db } from './models/index.js';
+
+(async () => {
+  try{
+    await db.mongoose.connect(db.url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('DB Connected');
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
+})();
+
 const app = express();
 
 app.use(medicineRouter);
